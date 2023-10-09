0040557eec4a443e5cc86279569c70e9fe05b9e4
Maxime Bartier <m.bartier@arkdevel.be>
2023-10-09 12:33:10 +0200

    ajout de commentaires

--- lab-2023/01_intro/functions.php
+++ lab-2023/01_intro/functions.php
@@ -1,17 +1,30 @@
 <?php
+// Cette function affiche un message de 'salutation'
+// récupération d'un text et retourne le un 'Salut ' suivi du text introduit
 function salut($nom) {
+  // impression du message par défaut suivit du text qu'on a ici appelé `$nom`
 	echo 'Salut ' . $nom;
 }
 
+// function qui calcule une moyenne 
+// on entre un tableau de chiffres `$nbrs`
 function moyenne($nbrs) {
+  // array_sum -> https://www.php.net/manual/en/function.array-sum.php
+  // count -> https://www.php.net/manual/en/function.count.php
+  // retourne une division des deux valeurs
 	return array_sum($nbrs) / count($nbrs);
 }
 
+// retourn si un age est 'OK' ou 'NON'
 function ageOk($age) {
-	if ($age > 18) {
-    	return 'OK';
-    }
-    return 'NON';
+  // condition -> si `$age` supérieur a `18`
+  if ($age > 18) {
+    // alors retourner 'OK'
+    return 'OK';
+  }
+
+  // sinon retourner 'NON'
+  return 'NON';
 }
 ?>
 
