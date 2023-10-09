7bb6e6dd242eb25bcf328ff8105930f784afb887
Maxime Bartier <m.bartier@arkdevel.be>
2023-10-09 11:47:59 +0200

    add lab-2023

---
+++ lab-2023/01_intro/array_students.php
@@ -0,0 +1,39 @@
+<?php
+$students = [
+	[
+    'name' => 'John',
+    'lastname' => 'Doe',
+    'note' => 13,
+  ], 
+  [
+    'name' => 'Jean',
+    'lastname' => 'DADA',
+    'note' => 11,
+  ], 
+  [
+    'name' => 'Tom',
+    'lastname' => 'OIOIH',
+    'note' => 9,
+  ], 
+];
+?>
+<html>
+<body>
+<ul>
+<?php foreach ($students as $student) { ?>
+<li>
+	<?php echo $student['name'] .' '.  $student['lastname']; ?>
+    <span
+     style="background-color:<?php if ($student['note'] < 10) {
+     	echo 'red';
+     }  else { 
+     	echo 'green'; 
+     } ?>;"
+    >
+    	<?php echo '(' . $student['note'] . ')'; ?>
+    </span>
+</li>
+<?php } ?>
+</ul>
+</body>
+</html>
