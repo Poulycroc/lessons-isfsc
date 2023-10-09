96040b4785bfd380df1c1ff57144b4111df16718
Maxime Bartier <m.bartier@arkdevel.be>
2023-10-09 15:33:51 +0200

    changes opti

--- lab-2023/01_intro/conditions.php
+++ lab-2023/01_intro/conditions.php
@@ -4,25 +4,24 @@ $age = 17;
 <html>
 <style>
 .red {
-background-color: red;
+  background-color: red;
 }
 .yellow {
-background-color: yellow;
+  background-color: yellow;
 }
 .font-color {
 color: green ;
 }
 </style>
 <body 
-class="font-color <?php if ($age >= 18) {echo 'red';} else { echo 'yellow'; }?>"
+  class="font-color <?php if ($age >= 18) {echo 'red';} else { echo 'yellow'; }?>"
 >
-<ul>
 
 <?php if ($age >= 18) { ?>
-<h1>ok</h1>
+  <h1>ok</h1>
 <?php } else { ?>
-<h1>NON</h1>
+  <h1>NON</h1>
 <?php } ?>
-</ul>
+
 </body>
 </html>
