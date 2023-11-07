6a146eb3c574e49aad88d69383dd8f834a1abaf6
Poulycroc <esga.retard@gmail.com>
2023-11-07 13:07:43 +0100

    clean

--- lab-2023/03_demo-wordpress/wp-content/themes/demo-them/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-  <span>
-    ISFSC Wordpress demo | 2023
-  </span>
-</footer>
-
-<?php wp_footer(); ?>
-</body>
-</html>
--- lab-2023/03_demo-wordpress/wp-content/themes/demo-them/functions.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-// Ajouter la prise en charge des images mises en avant
-add_theme_support( 'post-thumbnails' );
