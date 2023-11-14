9cf01b9409fa51e362c38bb8a36c5ca0edd13541
Poulycroc <esga.retard@gmail.com>
2023-11-14 10:19:19 +0100

    wip

---
+++ lab-2023/04_mon-cv-theme/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
